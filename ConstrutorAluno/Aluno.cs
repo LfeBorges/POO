7a0767a9f4d567d5af27a6804e9a775365ef3eed
Luiz Felipe <luiz.bore2004@gmail.com>
2024-09-18 16:30:55 -0300

    Construtor Aluno
    Terminando a atividade de construtor

--- ConstrutorAluno/Aluno.cs
+++ ConstrutorAluno/Aluno.cs
@@ -7,52 +7,40 @@ namespace ConstrutorAluno
 {
     public class Aluno
     {
-        private string aluno;
+        private string nome;
         private int matricula;
         private static int contador;
-    
-    }
-   /* public string Aluno
-    {
-        set 
-        {
-            this.aluno = aluno;
-        }
-        get
-        {
-            return aluno;
-        }
-    }
-    public int Matricula
-    {
-        set
-        {
-            this.matricula = matricula;
-        }
-        get
-        {
-            return matricula;
-        }
-    }
-    */
-    public static int Contador
-    {
-        get{return contador;}
-    }
 
-    public Aluno()
-    {
-        contador ++;
-    }
-    public Aluno(string nome)
-    {
-        this.nome = Nome;
-        contador ++;
-    }
-    public Aluno(string nome, int matricula)
-    {
-        this.nome = nome;
-        this.matricula = matricula;
-        contador ++;
+        public static int Contador
+        {
+            get{return contador;}
+        }
+
+        public Aluno()
+        {
+            contador ++;
+            this.matricula = GerarMatricula();
+        }
+        public Aluno(string nome)
+        {
+            this.nome = nome;
+            contador ++;
+            this.matricula = GerarMatricula();
+        }
+        public Aluno(string nome, int matricula)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+            contador ++;
+        }
+
+        private int GerarMatricula()
+        {
+            return 1570482313000 + contador;  
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine($"Nome: {nome}, Matrícula: {matricula}");
+        }
     }
 }
--- ConstrutorAluno/Program.cs
+++ ConstrutorAluno/Program.cs
@@ -1,18 +1,14 @@
-﻿internal class Program
-{
-    private static void Main(string[]args)
+﻿using ConstrutorAluno;
+    class Program
     {
-        //Criar a Classe Aluno, com os atributos
-        //Matricula, Nome
-        /* 
-         Utilizar os atributos static para criar as matriculas
-        de forma automática a cada instância seguindo o padão Fatec
-        1570482313000. 
-         O atributo matricula não será static, utilize
-        se quiser o atributo contador para auxiliar a contagem e registro
-        da matricula. 
-         Criar o método mostrar e chamar após cada instância
-         Instancie pelo menos 2 objetos de construtores diferentes
-        */
+        static void Main(string[] args)
+        {
+            
+            Aluno aluno1 = new Aluno("João");
+            aluno1.Mostrar();
+
+            Aluno aluno2 = new Aluno("Maria");
+            aluno2.Mostrar();
+
+        }
     }
-}
